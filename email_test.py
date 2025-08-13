ae51a26a93ac59ea1e9ea0512e09a330739a255a
brwngld <bern587@yahoo.com>
2025-08-13 09:15:34 +0000

    Update requirement.txt to include gunicorn

--- email_test.py
+++ email_test.py
@@ -2,7 +2,7 @@ import smtplib
 
 try:
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login("amegben@gmail.com", "xkatfvyyhdgjdjye")
+    server.login("your_email_address", "your_app_password")
     print("Logged in successfully!")
     server.quit()
 except Exception as e:
